fb49d3875ec279601433f0d1c6191f54f92c9f40
David Fuentes <dvj.fuentes@gmail.com>
2022-06-15 17:20:09 -0500

    First npm publish

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ng build ng-resman
+ng build ng-resman && cd dist/ng-resman && npm publish --access public
+
+
+
--- projects/ng-resman-test-app/src/app/providers/test-serv.service.ts
+++ projects/ng-resman-test-app/src/app/providers/test-serv.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BaseResourceManager } from 'dist/ng-resman';
+import { BaseResourceManager } from '@davefellow/ng-resource-manager';
 
 @Injectable({
   providedIn: 'root'
